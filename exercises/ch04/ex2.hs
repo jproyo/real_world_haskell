84f5af573245cec47c4e604f390339598f712b75
jproyo <jproyo@mxhero.com>
2015-04-01 17:23:59 -0300

    Adding exercises

--- exercises/ch04/ex2.hs
+++ exercises/ch04/ex2.hs
@@ -1,6 +1,7 @@
 -- file: ch04/ch04.exercises.hs
 
-import Data.Char (digitToInt)
+import Data.Char (digitToInt,isDigit)
+import Data.Either
 
 asInt_fold :: String -> Int
 asInt_fold [] = error "Not a valid number"
@@ -8,3 +9,15 @@ asInt_fold l@(x:xs) | x == '-' = negate (asInt_fold xs)
 										| otherwise = foldl convertAndSum 0 l
 	where convertAndSum acc x = acc * 10 + digitToInt x
 
+
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either [] = Left "Empty Input"
+asInt_either ('-':xs) = case result of 
+													Left err -> Left err
+													Right val -> Right (-val)
+	where result = asInt_either xs
+asInt_either xs = foldl convertAndSum (Right 0) xs
+  where convertAndSum (Right acc) x | (x >= '0') && (x <= '9') = Right (acc * 10 + digitToInt x)
+                                    | otherwise = Left ("Not a digit '" ++ [x] ++ "'")
+        convertAndSum (Left err) _ = Left err
