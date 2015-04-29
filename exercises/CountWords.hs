1a692351740b102ec93390d30ef15057016771cd
jproyo <jproyo@mxhero.com>
2015-04-29 16:22:55 -0300

    #NA: shortend code

--- exercises/CountWords.hs
+++ exercises/CountWords.hs
@@ -3,11 +3,10 @@ groupByLength l [] = []
 groupByLength l word | l > 0 = (take l word) : groupByLength l (drop l word)
 	| otherwise = []
 
-splitAndCount :: Int -> [Int] -> Int -> String -> Int
-splitAndCount _ _ _ [] = 0
-splitAndCount maxLengthWord groupsLength maximumTop word = maximumHead groupsLength maximumTop (take maxLengthWord word)
-    where maximumHead _ _ [] = 0 
-          maximumHead (x:xs) top wordTruncated = head (filter (<=top) ((length $ groupByLength x wordTruncated) : [maximumHead xs top wordTruncated]))
+splitAndCount :: [Int] -> Int -> String -> Int
+splitAndCount _ _ [] = 0
+splitAndCount groups top word = head (filter (<=top) (map lengthGroup groups))
+    where lengthGroup x = length $ groupByLength x word
 
 
 countWord :: Int -> Int -> Int -> Int -> String -> Int
@@ -15,6 +14,6 @@ countWord n k l m word
   | 2 > k || k > l || l > 26 = 0
   | m < 1 = 0
   | n < 1 = 0
-  | otherwise = splitAndCount n [k..l] m word
+  | otherwise = splitAndCount [k..l] m (take n word)
 
 main = print $ countWord 6 2 3 4 "ababab"
